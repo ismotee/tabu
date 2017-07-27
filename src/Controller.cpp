d476f67f0872cc65027e49752b6764922b857262
ismo <ismo@ismonKone>
2017-07-27 14:35:44 +0300

    OscInterface lisatty

--- src/Controller.cpp
+++ src/Controller.cpp
@@ -12,6 +12,7 @@ Controller::~Controller() {
 }
 
 void Controller::setup(float timesPerSecond) {
+    setAddress("localhost", 9997, 9998);
     Sessio::setup(300, 200);
     timedThread::setup(timesPerSecond);
     piirto::setup(ofGetWidth(), ofGetHeight());
@@ -19,18 +20,19 @@ void Controller::setup(float timesPerSecond) {
 
 void Controller::update() {
     Sessio::update();
-    if(Sessio::moodi == piirtaa)
+    if (Sessio::moodi == piirtaa) {
         piirto::update(viivanHallinta.nopeus);
-    else {
+        sendFloat("/nopeus",viivanHallinta.nopeus);
+    } else {
         double pi = 3.1415926535897;
-        float arvo = sin((float)(Sessio::updateCount%2000)/2000*pi);
-        
+        float arvo = sin((float) (Sessio::updateCount % 2000) / 2000 * pi);
+
         piirto::update(arvo);
     }
 }
 
 void Controller::loop() {
-    switch(Sessio::moodi) {
+    switch (Sessio::moodi) {
         case kalibroi:
             viivanHallinta.lisaaPisteKalibrointiin(hiiri.mouseState);
             viivanHallinta.kalibroi();
@@ -47,8 +49,8 @@ void Controller::loop() {
     }
 }
 
-void Controller::resize(int w_, int h_){
+void Controller::resize(int w_, int h_) {
     w = w_;
     h = h_;
-    piirto::setup(w,h);
+    piirto::setup(w, h);
 }
