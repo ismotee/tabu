074bca58138fdd763637fb1b5e021c0a03f19d9e
Matti Hyvönen <mattihyvonen@gmail.com>
2017-07-27 16:19:56 +0300

    obj poistettu

--- obj/linux64/Release/src/vector2d.d
+++
@@ -1,5 +0,0 @@
-obj/linux64/Release/src/vector2d.o: \
- /home/olento/koodaus/of_v0.9.8_linux64_release/apps/myApps/tabu/src/vector2d.cpp \
- /home/olento/koodaus/of_v0.9.8_linux64_release/apps/myApps/tabu/src/vector2d.h
-
-/home/olento/koodaus/of_v0.9.8_linux64_release/apps/myApps/tabu/src/vector2d.h:
